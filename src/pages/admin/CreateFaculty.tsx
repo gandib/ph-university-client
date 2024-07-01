52dda94bcd4a4c0c0f2df457e2446375fee576af
gandib <gandibroy11@gmail.com>
2024-07-01 22:23:12 +0600

    first commit

---
+++ src/pages/admin/CreateFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateFaculty = () => {
+  return (
+    <div>
+      <h1>Faculty</h1>
+    </div>
+  );
+};
+
+export default CreateFaculty;
---
+++ src/pages/admin/CreateStudent.tsx
@@ -0,0 +1,9 @@
+const CreateStudent = () => {
+  return (
+    <div>
+      <h1>Student</h1>
+    </div>
+  );
+};
+
+export default CreateStudent;
