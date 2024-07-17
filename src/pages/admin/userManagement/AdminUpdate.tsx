869aba2c0c251ee589b2f0901533937608962a36
gandib <gandibroy11@gmail.com>
2024-07-17 17:07:26 +0600

    admin completed

--- src/pages/admin/userManagement/AdminUpdate.tsx
+++ src/pages/admin/userManagement/AdminUpdate.tsx
@@ -1,8 +1,160 @@
+import { useParams } from "react-router-dom";
+import userManagementApi from "../../../redux/features/admin/userManagement.api";
+import { Button, Col, Divider, Row } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import PHSelect from "../../../components/form/PHSelect";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { toast } from "sonner";
+import { TResponse } from "../../../types";
+import { bloodGroupOptions, genderOptions } from "../../../constants/global";
+
 const AdminUpdate = () => {
+  const { adminId } = useParams();
+  const { data } = userManagementApi.useGetSingleStudentQuery(adminId);
+
+  console.log(data);
+
+  const adminDefaultValues = {
+    name: {
+      firstName: data?.data?.name?.firstName,
+      middleName: data?.data?.name?.middleName,
+      lastName: data?.data?.name?.lastName,
+    },
+    designation: data?.data?.designation,
+    gender: data?.data?.gender,
+    bloodGroup: data?.data?.bloodGroup,
+
+    email: data?.data?.email,
+    contactNo: data?.data?.contactNo,
+    emergencyContactNo: data?.data?.emergencyContactNo,
+    presentAddress: data?.data?.presentAddress,
+    permanentAddress: data?.data?.permanentAddress,
+  };
+
+  const [updateAdmin, { data: updatedData, error }] =
+    userManagementApi.useUpdateAdminMutation();
+  console.log({ updatedData, error });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+    const adminData = {
+      id: adminId,
+      data: {
+        password: data?.password,
+        admin: data,
+      },
+    };
+    // const formData = new FormData();
+    // formData.append("data", JSON.stringify(studentData));
+    // formData.append("file", data?.profileImg);
+
+    console.log(adminData);
+
+    const toastId = toast.loading("Loading...");
+
+    try {
+      const res = (await updateAdmin(adminData)) as TResponse<{
+        message: string;
+      }>;
+      console.log(res);
+      if (res?.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success(res?.data?.message, { id: toastId });
+      }
+    } catch (error) {
+      toast.error("Something went wrong!", { id: toastId });
+    }
+
+    // for development purpose to see data
+    // console.log(Object.fromEntries(formData));
+  };
+
   return (
-    <div>
-      <h1>Hello, AdminUpdate!</h1>
-    </div>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit} defaultValues={adminDefaultValues}>
+          <Divider>Personal Information</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHSelect name="gender" label="Gender" options={genderOptions} />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHDatePicker name="dateOfBirth" label="Date Of Birth" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHSelect
+                name="bloodGroup"
+                label="Blood Group"
+                options={bloodGroupOptions}
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="password" name="password" label="Password" />
+            </Col>
+            {/* <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <Controller
+                name="profileImg"
+                render={({ field: { onChange, value, ...field } }) => (
+                  <Form.Item label="Image">
+                    <Input
+                      type="file"
+                      value={value?.fileName}
+                      onChange={(e) => onChange(e.target.files?.[0])}
+                      {...field}
+                    />
+                  </Form.Item>
+                )}
+              />
+            </Col> */}
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="designation" label="Designation" />
+            </Col>
+
+            <Divider>Contact Information</Divider>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="email" label="Email" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput type="text" name="contactNo" label="Contact No" />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="emergencyContactNo"
+                label="Emergency Contact No"
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="presentAddress"
+                label="Present Address"
+              />
+            </Col>
+            <Col span={24} md={{ span: 12 }} lg={{ span: 8 }}>
+              <PHInput
+                type="text"
+                name="permanentAddress"
+                label="Permanent Address"
+              />
+            </Col>
+          </Row>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   );
 };
 
